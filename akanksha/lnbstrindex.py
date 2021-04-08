27a2158b89d2cd6a7c1f9e9faa7a95a62e157862
lnbspace <79511584+lnbspace@users.noreply.github.com>
2021-04-08 13:36:39 +0530

    Add files via upload

---
+++ akanksha/lnbstrindex.py
@@ -0,0 +1,14 @@
+str= input("enter a string:")
+
+a=print("length of the string is:",len(str))
+
+if a>7:
+    for c in str:
+        c=str[0:index]+2
+        print(c)
+elif a<=7:
+    for f in str:
+        f=str[1:index]+2
+        print(f)
+
+    
---
+++ akanksha/lnbsum.py
@@ -0,0 +1,36 @@
+
+lst = []
+n = int(input("Enter number of elements : "))
+for i in range(0, 5):
+    ele = int(input())
+  
+    lst.append(ele) 
+      
+print(lst)
+
+
+lst.remove(6)
+
+
+
+lst.remove(5)
+
+
+
+lst
+
+
+
+total=0
+for ele in range(0, len(lst)):
+    total = total + lst[ele]
+ 
+
+print("Sum of all elements in given list: ", total)
+
+
+
+
+
+
+
